cc94510d272cdb07d5c085a77acbc15020a8a616
allenINO03 <111472758+allenINO03@users.noreply.github.com>
2022-08-22 11:06:53 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,39 @@
+public class App {
+    public static void main(String[] args) {
+
+        Character knight = new Character();
+
+        knight.strength = 10;
+        knight.agility = 5;
+        knight.intelligence = 0;
+        knight.name = "Chen";
+        knight.sayMyName();
+
+        Character mage = new Character();
+
+        mage.strength = 0;
+        mage.agility = 5;
+        mage.intelligence = 10;
+        mage.name = "Akasha";
+        mage.sayMyName();
+
+        Character thief = new Character();
+
+        thief.strength = 5;
+        thief.agility = 10;
+        thief.intelligence = 0;
+        mage.name = "Gondar";
+        thief.sayMyName();
+
+        Character archer = new Character();
+
+        archer.strength = 5;
+        archer.agility = 5;
+        archer.intelligence = 5;
+        archer.name = "allen mitok";
+        archer.sayMyName();
+        archer.sayMyStrength();
+        archer.sayMyAgility();
+        archer.sayMyIntelligence();
+    }
+}
